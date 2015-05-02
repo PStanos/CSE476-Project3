c0a951d1529feb2478a89ebbadea75c1293c025b
PStanos <paulstanos@gmail.com>
2015-05-02 13:42:17 -0400

    Add point and set color options

--- app/src/main/java/edu/msu/stanospa/project3/DrawView.java
+++ app/src/main/java/edu/msu/stanospa/project3/DrawView.java
@@ -6,10 +6,37 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
-/**
- * Created by Paul on 5/2/2015.
- */
+import java.util.ArrayList;
+
 public class DrawView extends View {
+    public enum PaintColor {
+        Black,
+        White,
+        Red,
+        Green,
+        Blue
+    }
+
+    private class DrawPoint {
+        public static final float MAX_RADIUS = 25.0f;
+
+        public DrawPoint(float x, float y, float radius, Paint currentPaint) {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+            this.paint = currentPaint;
+        }
+
+        public void onDraw(Canvas canvas) {
+            canvas.drawCircle(x, y, radius, paint);
+        }
+
+        private float x;
+        private float y;
+        private float radius;
+        private Paint paint;
+    }
+
     private Paint greenPaint;
     private Paint blackPaint;
     private Paint bluePaint;
@@ -17,6 +44,8 @@ public class DrawView extends View {
     private Paint redPaint;
 
     private Paint currentPaint;
+
+    private ArrayList<DrawPoint> points = new ArrayList<DrawPoint>();
 
     public DrawView(Context context) {
         super(context);
@@ -61,6 +90,32 @@ public class DrawView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawCircle(100.0f, 100.0f, 25.0f, currentPaint);
+        for(DrawPoint drawPoint : points) {
+            drawPoint.onDraw(canvas);
+        }
+    }
+
+    public void addPoint(float scaledX, float scaledY, float scaledRadius) {
+        points.add(new DrawPoint(scaledX * this.getWidth(), scaledY * this.getHeight(), scaledRadius * DrawPoint.MAX_RADIUS, currentPaint));
+    }
+
+    public void setPaintColor(PaintColor chosenColor) {
+        switch(chosenColor) {
+            case Black:
+                currentPaint = blackPaint;
+                break;
+            case White:
+                currentPaint = whitePaint;
+                break;
+            case Red:
+                currentPaint = redPaint;
+                break;
+            case Green:
+                currentPaint = greenPaint;
+                break;
+            case Blue:
+                currentPaint = bluePaint;
+                break;
+        }
     }
 }
