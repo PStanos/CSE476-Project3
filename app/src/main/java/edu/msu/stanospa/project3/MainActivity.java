51d71b7c23c85f07509ff623710009b02b3e898c
Kevin Pauly <kevin@kevinpauly.me>
2015-05-02 13:52:07 -0400

    colors

--- app/src/main/java/edu/msu/stanospa/project3/MainActivity.java
+++ app/src/main/java/edu/msu/stanospa/project3/MainActivity.java
@@ -1,39 +1,113 @@
 package edu.msu.stanospa.project3;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private Sensor accelSensor = null;
+    private AccelListener accelListener = null;
+    private float radius;
+    private float y;
+
+    private static float A = 0.95f;
+    private static float MAX_Y = 9.5f;
+    private static float MIN_Y = 0f;
+    private static int DELAY = 50;
+    private DrawView drawView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawView = (DrawView)this.findViewById(R.id.drawView);
+
+        SensorManager sensorManager = (SensorManager)this.getSystemService(Context.SENSOR_SERVICE);
+        accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if(accelSensor != null) {
+            accelListener = new AccelListener();
+            sensorManager.registerListener(accelListener,
+                    accelSensor,
+                    SensorManager.SENSOR_DELAY_GAME);
+        }
+
+        executeOnDelay();
     }
 
+    private class AccelListener implements SensorEventListener {
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            y = (1 - A) * event.values[1] + A * y;
+            if(y > MAX_Y)
+                y = MAX_Y;
+            if(y < MIN_Y)
+                y = MIN_Y;
+            radius = y / MAX_Y;
+        }
+    }
+
+    public void executeOnDelay() {
+        drawView.addPoint(0,0,radius);
+        delay();
+    }
+
+    public void delay() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                executeOnDelay();
+            }
+        }, DELAY);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.color_black:
+                drawView.setPaintColor(DrawView.PaintColor.Black);
+                return true;
+            case R.id.color_white:
+                drawView.setPaintColor(DrawView.PaintColor.White);
+                return true;
+            case R.id.color_red:
+                drawView.setPaintColor(DrawView.PaintColor.Red);
+                return true;
+            case R.id.color_green:
+                drawView.setPaintColor(DrawView.PaintColor.Green);
+                return true;
+            case R.id.color_blue:
+                drawView.setPaintColor(DrawView.PaintColor.Blue);
+                return true;
+            case R.id.send_picture:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
